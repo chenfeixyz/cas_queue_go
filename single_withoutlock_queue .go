9646126201aec52e8f413ae8dbcaafd4df62de5a
chenfei <88298113@qq.com>
2023-05-22 15:05:16 +0800

    泛型
    泛型

--- single_withoutlock_queue .go
+++ single_withoutlock_queue .go
@@ -7,8 +7,8 @@ import "sync/atomic"
  * without locks.
  */
 
-type queueNode struct {
-	data interface{}
+type queueNode[T any] struct {
+	data T
 	flag uint32
 }
 
@@ -23,31 +23,28 @@ func RoundUp(v uint32) uint32 {
 	return v
 }
 
-type SingleWithoutLockQueue struct {
+type SingleWithoutLockQueue[T any] struct {
 	_pad0      [64]byte
 	size       uint32
 	capMod     uint32
 	_pad1      [64]byte
-	records    []*queueNode
+	records    []queueNode[T]
 	_pad2      [64]byte
 	readIndex  uint32
 	_pad3      [64]byte
 	writeIndex uint32
 }
 
-func NewSingleWithoutLockQueue(capacity uint32) *SingleWithoutLockQueue {
-	q := new(SingleWithoutLockQueue)
+func NewSingleWithoutLockQueue[T any](capacity uint32) *SingleWithoutLockQueue[T] {
+	q := new(SingleWithoutLockQueue[T])
 	q.size = RoundUp(capacity)
 	q.capMod = q.size - 1
-	q.records = make([]*queueNode, q.size)
-	for i := uint32(0); i < q.size; i++ {
-		q.records[i] = &queueNode{}
-	}
+	q.records = make([]queueNode[T], q.size)
 	return q
 }
 
-func (m *SingleWithoutLockQueue) Write(value interface{}) bool {
-	n := m.records[m.writeIndex]
+func (m *SingleWithoutLockQueue[T]) Write(value T) bool {
+	n := &m.records[m.writeIndex]
 	if 1 == atomic.LoadUint32(&n.flag) {
 		return false
 	}
@@ -58,29 +55,28 @@ func (m *SingleWithoutLockQueue) Write(value interface{}) bool {
 	return true
 }
 
-func (m *SingleWithoutLockQueue) Read() (v interface{}, b bool) {
-	n := m.records[m.readIndex]
+func (m *SingleWithoutLockQueue[T]) Read() (ret T, ok bool) {
+	n := &m.records[m.readIndex]
 	if 0 == atomic.LoadUint32(&n.flag) {
-		return nil, false
+		return ret, false
 	}
 
-	v = n.data
-	n.data = nil
+	ret, n.data = n.data, ret
 	n.flag = 0
 
 	m.readIndex = (m.readIndex + 1) & m.capMod
-	return v, true
+	return ret, true
 }
 
-func (m *SingleWithoutLockQueue) IsEmpty() bool {
-	n := m.records[m.readIndex]
+func (m *SingleWithoutLockQueue[T]) IsEmpty() bool {
+	n := &m.records[m.readIndex]
 	if 0 == atomic.LoadUint32(&n.flag) {
 		return true
 	}
 	return false
 }
 
-func (m *SingleWithoutLockQueue) Count() uint32 {
+func (m *SingleWithoutLockQueue[T]) Count() uint32 {
 	w := atomic.LoadUint32(&m.writeIndex)
 	r := atomic.LoadUint32(&m.readIndex)
 	if w >= r {
@@ -90,6 +86,6 @@ func (m *SingleWithoutLockQueue) Count() uint32 {
 	}
 }
 
-func (m *SingleWithoutLockQueue) Capacity() uint32 {
+func (m *SingleWithoutLockQueue[T]) Capacity() uint32 {
 	return m.size
 }
